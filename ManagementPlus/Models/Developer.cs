8977995b62884f9b5d586c8cad29238803df72f3
gerardnts <87734279+gerardnts@users.noreply.github.com>
2021-10-22 23:24:42 -0400

    feat: define individual contributor entity

--- ManagementPlus/Models/Developer.cs
+++ ManagementPlus/Models/Developer.cs
@@ -1,13 +1,10 @@
 ﻿using ManagementPlus.Enums;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ManagementPlus.Models
 {
-    public class Developer
+    public class IndividualContributor
     {
         public string FullName { get; set; }
         public int Level { get; set; }
