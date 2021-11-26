28bac5389347959ea272254e4f123bbe9ac9faa0
lunox94 <gmrlunox94@gmail.com>
2021-11-26 13:20:02 -0500

    chore: refactor hour reports routing

--- ManagementPlus/Pages/HourReports/Index.cshtml.cs
+++ ManagementPlus/Pages/HourReports/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,8 +25,20 @@ namespace ManagementPlus.Pages.HourReports
         }
 
         public IList<HourReport> HourReport { get;set; }
+        [BindProperty]
         public ProjectVM Project { get; set; }
+        [BindProperty]
         public IndividualContributorVM IndividualContributor { get; set; }
+        [BindProperty]
+        [Display(Name = "Report Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReportDate { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Year { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Month { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Day { get; set; }
 
         public async Task<IActionResult> OnGetAsync(Guid? projectId, Guid? individualContributorId)
         {
@@ -43,13 +56,32 @@ namespace ManagementPlus.Pages.HourReports
             }
 
             Project = _mapper.Map<ProjectVM>(project);
-            IndividualContributor = _mapper.Map<IndividualContributorVM>(individualContributor);
+            IndividualContributor = _mapper.Map<IndividualContributorVM>(individualContributor);            
+
+            ReportDate = new DateTime(year: Year, month: Month, day: Day);
 
             HourReport = await _context.HourReports
-                .Where(hr => hr.ProjectId == projectId && hr.IndividualContributorId == individualContributorId)
+                .Where
+                (
+                    hr => hr.ProjectId == projectId
+                    && hr.IndividualContributorId == individualContributorId
+                    && hr.DateOfIssue == ReportDate
+                )
                 .ToListAsync();
-            
+
             return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("./Index", routeValues: new
+            {
+                projectId = Project.Id,
+                individualContributorId = IndividualContributor.Id,
+                year = ReportDate.Year,
+                month = ReportDate.Month,
+                day = ReportDate.Day
+            });
         }
     }
 }
