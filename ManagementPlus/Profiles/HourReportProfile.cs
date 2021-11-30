dca0a5575efb4e72182d128de61c3e8ee628bfcb
lunox94 <gmrlunox94@gmail.com>
2021-11-29 21:19:20 -0500

    feat: add mapping for hour report to create

---
+++ ManagementPlus/Profiles/HourReportProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ManagementPlus.Extensions;
+using ManagementPlus.Models;
+using ManagementPlus.ViewModels;
+
+namespace ManagementPlus.Profiles
+{
+    public class HourReportProfile : Profile
+    {
+        public HourReportProfile()
+        {
+            CreateMap<HourReportToCreateVM, HourReport>().ForMember(
+                dest => dest.ReportedTime,
+                opt => opt.MapFrom(src => src.ReportedTime.ToTimeSpanTicks())
+            ).ForMember(
+                dest => dest.DiscountTime,
+                opt => opt.MapFrom(src => src.DiscountTime.ToTimeSpanTicks())
+            );
+        }
+    }
+}
