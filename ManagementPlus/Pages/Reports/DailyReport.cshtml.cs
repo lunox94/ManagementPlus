f6025b5957570c13436cb6554f6ec0ba03b65676
lunox94 <gmrlunox94@gmail.com>
2021-12-11 13:55:33 -0500

    feat: implement proper navigation to daily report page

--- ManagementPlus/Pages/Reports/DailyReport.cshtml.cs
+++ ManagementPlus/Pages/Reports/DailyReport.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,24 @@ namespace ManagementPlus.Pages.Reports
 {
     public class DailyReportModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        [Display(Name = "Report Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReportDate { get; set; }
+
+        public void OnGet(int year, int month, int day)
         {
+            ReportDate = new DateTime(year: year, month: month, day: day);
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("./DailyReport", routeValues: new
+            {
+                year = ReportDate.Year,
+                month = ReportDate.Month,
+                day = ReportDate.Day
+            });
         }
     }
 }
